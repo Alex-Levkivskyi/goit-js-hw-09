16b3bf7e61fd1d8c5fbff8e69bfafec6b26e32fa
Alex_L <alexandrrbatkovich@gmail.com>
2026-08-22 22:26:16 -0700

    Fix simplelightbox css import path

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,6 +1,6 @@
 
 import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-light-box.min.css";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const images = [
   {
